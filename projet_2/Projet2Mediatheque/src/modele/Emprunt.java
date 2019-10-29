35004e476706f7205950de53e4db5916d55d2799
gabb134 <cg.marrero@cgodin.qc.ca>
2019-10-29 18:47:12 -0400

    modif gab prdi 311 29 oct 2019

---
+++ projet_2/Projet2Mediatheque/src/modele/Emprunt.java
@@ -0,0 +1,15 @@
+package modele;
+
+import java.io.Serializable;
+
+public class Emprunt implements Serializable{
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ projet_2/Projet2Mediatheque/src/modele/Prepose.java
@@ -0,0 +1,38 @@
+package modele;
+
+import java.io.Serializable;
+
+public class Prepose implements Serializable{
+
+	private int intNumEmploye;
+	private String strMotDePasse;
+	private String strAdresse;
+	private String strNom;
+	private String strPrenom;
+	private String strTelephone;
+	public Prepose(int intNumEmploye, String strMotDePasse, String strAdresse, String strNom, String strPrenom,
+			String strTelephone) {
+		super();
+		this.intNumEmploye = intNumEmploye;
+		this.strMotDePasse = strMotDePasse;
+		this.strAdresse = strAdresse;
+		this.strNom = strNom;
+		this.strPrenom = strPrenom;
+		this.strTelephone = strTelephone;
+	}
+	
+	
+	public void afficherAdherents() {
+		
+	}
+	
+	public void ajouterAdherent() {
+		
+	}
+	public void supprimerAdherent() {
+		
+	}
+	
+	
+	
+}
