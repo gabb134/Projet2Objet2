df5b2effc24a6589d093d08305cdc15f30245fc7
gabb134 <cg.marrero@cgodin.qc.ca>
2019-11-03 21:35:33 -0500

    modif gab ordi mac 3 nov 2019(soir)

--- projet_2/Projet2Mediatheque/src/modele/Adherent.java
+++ projet_2/Projet2Mediatheque/src/modele/Adherent.java
@@ -4,11 +4,43 @@ public class Adherent {
 private String strNom;
 private String strPrenom;
 private String strNumeroTelephone;
+
 public Adherent (String strNom,String strPrenom,String strNumeroTelephone){
 	this.strNom=strNom;
 	this.strPrenom=strPrenom;
 	this.strNumeroTelephone=strNumeroTelephone;
 }
+
+public String getStrNom() {
+	return strNom;
+}
+
+
+public void setStrNom(String strNom) {
+	this.strNom = strNom;
+}
+
+
+public String getStrPrenom() {
+	return strPrenom;
+}
+
+
+public void setStrPrenom(String strPrenom) {
+	this.strPrenom = strPrenom;
+}
+
+
+public String getStrNumeroTelephone() {
+	return strNumeroTelephone;
+}
+
+
+public void setStrNumeroTelephone(String strNumeroTelephone) {
+	this.strNumeroTelephone = strNumeroTelephone;
+}
+
+
 
 
 	public static void main(String[] args) {
--- projet_2/Projet2Mediatheque/src/modele/Prepose.java
+++ projet_2/Projet2Mediatheque/src/modele/Prepose.java
@@ -1,6 +1,14 @@
 package modele;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Prepose implements Serializable{
 
@@ -10,6 +18,8 @@ public class Prepose implements Serializable{
 	private String strNom;
 	private String strPrenom;
 	private String strTelephone;
+	
+	private ArrayList<Adherent> arrAdherent = new ArrayList<Adherent>();
 	public Prepose(String strNumEmploye, String strMotDePasse, String strAdresse, String strNom, String strPrenom,
 			String strTelephone) {
 		super();
@@ -20,18 +30,87 @@ public class Prepose implements Serializable{
 		this.strPrenom = strPrenom;
 		this.strTelephone = strTelephone;
 	}
-	
-	
-	public void afficherAdherents(Adherent adherent) {
-		
+	public Prepose() {
 	}
 	
+	//Gestion adhérents
+
+	
+	@SuppressWarnings("unchecked")
 	public void ajouterAdherent(Adherent adherent) {
+		File fichierAdherents= new File("C:/Users/GabrielMarrero/Downloads/test/fichierPreposes.ser");
+		//File fichierPreposes= new File("C:/Users/cg.marrero/Downloads/test/fichierPreposes.ser");
+		//File fichierPreposes= new File("C:/Users/rn.merzius/Downloads/test/fichierPreposes.ser");
+		
+		if(!fichierAdherents.exists()) {//Ajout de l'adherent
+			arrAdherent.add(adherent);
+			
+			//Serialization
+			try {
+				FileOutputStream fichier = new FileOutputStream(fichierAdherents);
+				ObjectOutputStream sortie = new ObjectOutputStream(fichier);
+				sortie.writeObject(arrAdherent);
+				sortie.close();
+				fichier.close();
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		else { //Verification pour voir si l'adherent a ete enregistrer
+			
+			//Deserialization
+
+			try {
+				FileInputStream fichier = new FileInputStream(fichierAdherents);
+				ObjectInputStream entree = new ObjectInputStream(fichier);
+
+				arrAdherent = (ArrayList<Adherent>) entree.readObject();
+				fichier.close();
+				entree.close();
+				
+				for(Adherent a:arrAdherent)
+					System.out.println(a);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			
+			
+	
+			
+			
+			
+		}
 		
 	}
 	public void supprimerAdherent(Adherent adherent) {
 		
 	}
+	public void afficherAdherents(Adherent adherent) {
+		
+	}
+	//Gestion catalogue
+	public void ajouterDocument(Document document) {
+		
+	}
+	public void supprimerDocument(Document document) {
+		
+	}
+	//Gestion des prêts 
+	public void inscrireUnPret() {
+		
+	}
+	public void inscrireUnRetour() {
+		
+	}
+	
+	
 	public String getNoEmploye()
 	{
 		return strNumEmploye;
@@ -39,6 +118,12 @@ public class Prepose implements Serializable{
 	public String getPrenom()
 	{
 		return strPrenom;
+	}
+	
+	public static void main(String[] args) {
+		Prepose p = new Prepose();
+		p.ajouterAdherent(new Adherent("Marrero", "Gabriel", "(514) 972-1014"));
+		
 	}
 	
 }
