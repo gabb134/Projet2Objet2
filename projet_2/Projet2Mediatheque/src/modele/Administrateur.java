8cf7a17ad7735a4f8b213a48b3773c6753859865
ribensky <rn.merzius@cgodin.qc.ca>
2019-11-14 19:48:29 -0500

    modif par Rib 14 novembre 2019 ajout de la classe liste Prepose et modifications de la classe admin

--- projet_2/Projet2Mediatheque/src/modele/Administrateur.java
+++ projet_2/Projet2Mediatheque/src/modele/Administrateur.java
@@ -9,83 +9,71 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class Administrateur {
-	
-	static ArrayList<Prepose> lstPreposes=new ArrayList<Prepose>();
-@SuppressWarnings("unchecked")
-public void AjouterPrepose(String strNom, String strPrenom, String strAdresse, String strTelephone, String strMotDePasse )
-{
-	//int intNumeroPrepose=1900;
-	
-	//File fichierPreposes= new File("/Users/r.merzius/Desktop/fichierPreposes.ser");
-	File fichierPreposes= new File("C:/Users/rn.merzius/Downloads/test/fichierPreposes.ser");
-	//File fichierPreposes= new File("C:/Users/GabrielMarrero/Downloads/test/fichierPreposes.ser");
-	//File fichierPreposes= new File("C:/Users/cg.marrero/Downloads/test/fichierPreposes.ser");
-	int intNumeroEmploye=0; 
-	int intNumAjout=19000;
-	if(fichierPreposes.exists())
+
+	static ArrayList<Prepose> lstPreposes=ListePreposes.getInstance().getLstPreposes();
+	@SuppressWarnings("unchecked")
+	public void AjouterPrepose(String strNom, String strPrenom, String strAdresse, String strTelephone, String strMotDePasse )
 	{
-		try {
+		//int intNumeroPrepose=1900;
 
-			// d�s�rialisation des pr�pos�s
-			FileInputStream fichier = new FileInputStream(fichierPreposes);
+		//File fichierPreposes= new File("/Users/r.merzius/Desktop/fichierPreposes.ser");
+		//File fichierPreposes= new File("preposes.ser");
+		//File fichierPreposes= new File("C:/Users/GabrielMarrero/Downloads/test/fichierPreposes.ser");
+		//File fichierPreposes= new File("C:/Users/cg.marrero/Downloads/test/fichierPreposes.ser");
+		int intNumeroEmploye=0; 
+		int intNumAjout=19000;
+		try{
+			if(lstPreposes.get(0)!=null)
+			{
 
-			ObjectInputStream entree = new ObjectInputStream(fichier);
 
-			lstPreposes = (ArrayList<Prepose>) entree.readObject();
-			fichier.close();
-			entree.close();
-			intNumeroEmploye=Integer.parseInt(lstPreposes.get(lstPreposes.size()-1).getNoEmploye().substring(1));
-			intNumeroEmploye++;
-			System.out.println(intNumeroEmploye);
-			lstPreposes.add(new Prepose("P"+intNumeroEmploye, strMotDePasse, strAdresse, strNom, strPrenom, strTelephone));
-			
-			
-			
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+
+				intNumeroEmploye=Integer.parseInt(lstPreposes.get(lstPreposes.size()-1).getNoEmploye().substring(1));
+				intNumeroEmploye++;
+				System.out.println(intNumeroEmploye);
+				lstPreposes.add(new Prepose("P"+intNumeroEmploye, strMotDePasse, strAdresse, strNom, strPrenom, strTelephone));
+
+
+
+
+
+			}
+
 		}
-		
+		catch (Exception e) {
+			lstPreposes.add(new Prepose("P"+Integer.toString(intNumAjout), strMotDePasse, strAdresse, strNom, strPrenom, strTelephone));}
+
+		ListePreposes.serialisationPrepose();
 	}
-	
-	else
+
+	public void connexion() 
 	{
-		lstPreposes.add(new Prepose("P"+Integer.toString(intNumAjout), strMotDePasse, strAdresse, strNom, strPrenom, strTelephone));
-	}
-	// S�rialisation des pr�pos�s
-	try {
-		FileOutputStream fichier = new FileOutputStream(fichierPreposes);
-		ObjectOutputStream sortie = new ObjectOutputStream(fichier);
-		sortie.writeObject(lstPreposes);
-		sortie.close();
-		fichier.close();
 
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-}
 
-public void connexion() 
-{
-	
-	
-}
-public static ArrayList<Prepose> getlstPreposes()
-{
-	return lstPreposes;
-	
-}
+	}
+	public static Boolean supprimerPrepose(String strNumeroEmploye)
+	{
+		Boolean booTrouver=false;
+		for(int i=0;i<lstPreposes.size();i++)
+		{
+			if(lstPreposes.get(i).getNoEmploye().equals(strNumeroEmploye))
+			{
+				lstPreposes.remove(i);
+				booTrouver=true;
+			}
+		}
+		ListePreposes.serialisationPrepose();
+		return booTrouver;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Administrateur admin=new Administrateur();
 		admin.AjouterPrepose("merzius", "paul", "inconnue", "(111) 111-1111", "Password1");
-		System.out.println(Administrateur.lstPreposes.get(0).getNoEmploye());
-		
+		System.out.println(Administrateur.lstPreposes.get(5).getNoEmploye());
 
+		if(Administrateur.supprimerPrepose("P19002"))
+			System.out.println("Supprimé avec succès");
 
 	}
 
---
+++ projet_2/Projet2Mediatheque/src/modele/ListePreposes.java
@@ -0,0 +1,146 @@
+package modele;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ListePreposes implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private ArrayList<Prepose> lstPreposes = new ArrayList<Prepose>();
+	private static ListePreposes instance;
+	
+	
+	
+
+
+	@SuppressWarnings("unchecked")
+	private ListePreposes() {
+		//deserialisation
+		File fichierPreposes= new File("preposes.ser");
+		
+		if(fichierPreposes.exists())
+		{
+			try {
+
+				// deserialisation des adherents
+				FileInputStream fichier = new FileInputStream(fichierPreposes);
+
+				ObjectInputStream entree = new ObjectInputStream(fichier);
+
+				lstPreposes = 	(ArrayList<Prepose>) entree.readObject(); 
+				fichier.close();
+				entree.close();
+ 				//intNumeroAdherent = Integer.parseInt(liste.getLstAdherents().get(liste.getLstAdherents().size()-1).getStrNumeroAdherent().substring(1));
+				//intNumeroAdherent++;
+				
+				
+				
+				
+				
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			//e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		
+			
+		}
+		else {
+			System.out.println("fichier n'existe pas");
+		}
+	}
+	public ArrayList<Prepose> getLstPreposes() {
+		return lstPreposes;
+	}
+
+	public static ListePreposes getInstance() {
+		if (instance == null) {
+			//
+			  instance = new ListePreposes();
+		}
+			
+			  return instance; 
+		
+	}
+	
+	public static void serialisationPrepose() {
+		
+		
+			try { 
+			      
+				FileOutputStream fichier = new FileOutputStream("preposes.ser"); 
+			      
+				ObjectOutputStream os = new ObjectOutputStream(fichier); 
+				
+				/*ArrayList<Adherent> lstAdherent = new ArrayList<Adherent>();
+				lstAdherent.add(a1);
+				lstAdherent.add(a2);*/
+				
+				 os.writeObject(ListePreposes.getInstance().getLstPreposes());
+				
+				 
+				
+			
+				
+			     os.close();
+				 System.out.println(ListePreposes.getInstance().getLstPreposes());
+				} 
+				    
+				catch (IOException e) { 
+				     
+				e.printStackTrace(); 
+				    
+				}
+		
+	}
+
+	public void setLstPreposes(ArrayList<Prepose> lstPreposes) {
+		this.lstPreposes = lstPreposes;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//prepose.ajouterAdherent("merzius", "paul", "Aucune adresse" , "(111) 111-1111", 0,0);
+		//prepose.ajouterAdherent("marrero", "gab","Aucune adresse" ,"(222) 222-2222",0,0);
+		
+		Prepose p1 = new Prepose("P19000","merzius", "paul", "inconnue", "(111) 111-1111", "Password1");
+		Prepose p2 = new Prepose("P19001","marrero", "gab", "inconnue", "(222) 222-2222", "Password1");
+		
+		try { 
+		      
+			FileOutputStream fichier = new FileOutputStream("adherent.ser"); 
+		      
+			ObjectOutputStream os = new ObjectOutputStream(fichier); 
+			
+			ArrayList<Prepose> lstPreposes = new ArrayList<Prepose>();
+			lstPreposes.add(p1);
+			lstPreposes.add(p2);
+			
+			 os.writeObject(lstPreposes);
+			
+			 
+			
+		System.out.println("qllo");
+			
+		     os.close();
+			} 
+			    
+			catch (IOException e) { 
+			     
+			e.printStackTrace(); 
+			    
+			} 
+		
+		System.out.println(ListePreposes.getInstance().getLstPreposes());
+
+	}
+
+}
